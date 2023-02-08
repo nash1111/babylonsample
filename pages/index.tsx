ef21cdfa2e82ea5138ddf972079a38af4c49cc21
nash1111 <ken1251213@gmail.com>
2023-02-09 08:23:04 +0900

    fix: css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Link href="/ball"> <a>球体と平面</a> </Link>
       <Link href="/kusoinakabuilding"> <a>家</a> </Link>
-      
     </>
   )
 }

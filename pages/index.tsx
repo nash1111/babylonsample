d4042ee2c8cbbccefaa486a3ce24f355bfeca1da
nash1111 <ken1251213@gmail.com>
2023-02-09 08:26:36 +0900

    fix link

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
+      <Link href="/square"> <a>赤い平面</a> </Link>
+      <br />
       <Link href="/ball"> <a>球体と平面</a> </Link>
+      <br />
       <Link href="/kusoinakabuilding"> <a>家</a> </Link>
     </>
   )
